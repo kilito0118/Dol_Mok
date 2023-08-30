54b64db737e42d3bd76787544a848c8702c00f87
kilito0118 <kilito0118@naver.com>
2023-08-30 19:16:15 +0900

    게임 완성

--- Dol_Mok/testpygame.py
+++ Dol_Mok/testpygame.py
@@ -17,6 +17,127 @@ force = [([400, 870], [500, 870], [450, 830]),
 pygame.display.set_caption("돌목")
 flag = False
 dr = 0
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+
+def before_start():
+    
+
+    global flag, dr
+    pygame.init()
+    font = pygame.font.Font("Dol_Mok\Chilgok_Cye.ttf",80)
+
+    SURFACE = pygame.display.set_mode((1200,900))
+    FPSCLOCK = pygame.time.Clock()
+    
+    for i in range(7):
+        for j in range(7):
+            pan[i][j]=-1 
+  
+    pygame.display.set_caption("돌목")
+    flag = False
+    dr = 0
+    dx = [0,0,-1,1]
+    dy = [1,-1,0,0]
+
+
+def check(x,y):
+    i,j = x,y
+    if y<4:
+
+        for i in range(4):
+            if pan[x][y+i]!=pan[x][y]:
+                break
+        else:
+            return pan[x][y]
+            print(1)
+    
+    if x>3: 
+        return
+    for i in range(4):
+        if pan[x+i][y]!=pan[x][y]:
+            break
+    else:
+        return pan[x][y]
+    if y<4:
+        for i in range(4):
+            if pan[x+i][y+i]!=pan[x][y]:
+                break
+        else:
+            return pan[x][y]
+        
+    if y<4:
+        for i in range(4):
+            if pan[x+i][y-i]!=pan[x][y]:
+                break
+        else:
+            return pan[x][y]
+
+        
+def show_dol():
+        for i in range(7):
+            for j in range(7):
+                if pan[i][j]==1:
+                    pygame.draw.circle(SURFACE,Dol_Color[1], ((j+1)*100+50,(i+1)*100+50), 40)
+                elif pan[i][j]==0:
+                    pygame.draw.circle(SURFACE,Dol_Color[0], ((j+1)*100+50,(i+1)*100+50), 40)
+        
+        for xpos in range(100,900,100):
+            pygame .draw.line(SURFACE, 0xFFFFFF, (xpos, 100), (xpos ,800))
+
+        for ypos in range(100,900,100):
+            pygame.draw.line(SURFACE, 0xFFFFFF, (100,ypos), (800,ypos))        
+
+
+def win():
+    count = 0
+    for i in range(7):
+        for j in range(7):
+            if pan[i][j] != -1:
+                a = check(i,j) 
+                if a != None:
+                    
+                    return a     
+                               
+                
+
+def game_end(k):
+    if k != None:
+        show_dol()
+        if k==0:
+            k = "Red"
+        else:
+            k = "Yellow"
+        text_Title= font.render(k+"win", True, 0xd0fc5c)
+        
+        SURFACE.blit(text_Title,[400,400])
+        text = font.render("Press the SPACE to Restart", True,0xd0fc5c)
+        SURFACE.blit(text,[200,500])
+        pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key==pygame.K_SPACE:
+                        before_start()
+                        print(1)
+                        show_dol()
+                        main()
+                        print(1)
+
+
+
+
+            
+        
+
+
+
+
+
 def gravity(Direction : int):
     for i in range(7):
         match Direction:
@@ -51,10 +172,11 @@ def gravity(Direction : int):
 def main():
     global dr, flag
     cnt=0
-
+    print(11)
 
 
     while True:
+        print(pan)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -71,16 +193,21 @@ def main():
                         
                 else:
                     continue
-                cnt+=1
+                
             elif event.type == pygame.KEYDOWN and flag == True:
                 if event.key==pygame.K_LEFT:
                     dr-=1
                     flag = False
-
+                    cnt+=1
                 elif event.key==pygame.K_RIGHT:
                     dr+=1
                     flag = False
-                
+                    cnt+=1
+                elif event.key == pygame.K_DOWN:
+                    flag = False
+                    cnt+=1
+        game_end(win())
+        
         if dr==-1:
             dr = 3
         elif dr==4:
@@ -92,22 +219,10 @@ def main():
             text_Title= font.render("Yellow 턴", True, 0xd0fc5c)
         pygame.draw.polygon(SURFACE, 0xd0fc5c, force[dr])
         SURFACE.blit(text_Title,[850,80])
+        show_dol()
         gravity(dr)
-        for i in range(7):
-            for j in range(7):
-                if pan[i][j]==1:
-                    pygame.draw.circle(SURFACE,Dol_Color[1], ((j+1)*100+50,(i+1)*100+50), 40)
-                elif pan[i][j]==0:
-                    pygame.draw.circle(SURFACE,Dol_Color[0], ((j+1)*100+50,(i+1)*100+50), 40)
+        game_end(win())
         
-        for xpos in range(100,900,100):
-            pygame .draw.line(SURFACE, 0xFFFFFF, (xpos, 100), (xpos ,800))
-
-        for ypos in range(100,900,100):
-            pygame.draw.line(SURFACE, 0xFFFFFF, (100,ypos), (800,ypos))
-
-        
-                    
 
         pygame.display.update()
         FPSCLOCK.tick(60)
